38239784c25af43893e0ff02521c446bc64b38b8
viniciusbenicio <vinicius.benicio97@gmail.com>
2022-12-27 19:12:24 -0300

    Importando o componente

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
 import './App.css';
+
+import FirstComponents from './components/FirstComponents';
 
 function App() {
   return (
     <div className="App">
-     <h1> Fundamentos React</h1>
+      <h1> Fundamentos React</h1>
+
+      <div>
+        <p>Components importado</p>
+        <FirstComponents />
+      </div>
+
     </div>
   );
 }
